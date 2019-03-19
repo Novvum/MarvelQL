59bd62dd120f2f36dcb4903e006c946dbc552e56
Rohit <rohit@novvuminc.com>
2019-03-19 14:21:53 -0700

    cached

--- packages/data/src/objects.ts
+++ packages/data/src/objects.ts
@@ -9,7 +9,7 @@ import { URL } from 'url';
 import qs from 'qs';
 import { Agent } from 'https';
 import { existsSync, writeFileSync } from 'fs';
-import { hash, ts, apikey, baseURL } from '@marvelql/graphql/utils';
+import { hash, ts, apikey, baseURL } from '../../graphql/utils/index';
 import fetch from 'isomorphic-fetch';
 const state = require("./data/_state.json")
 
