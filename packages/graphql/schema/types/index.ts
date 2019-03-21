e4389cb00e6ba88251c7e204cff615b6ec1584a8
Rohit <rohit@novvuminc.com>
2019-03-20 23:55:38 -0700

    auto import types

--- packages/graphql/schema/types/index.ts
+++ packages/graphql/schema/types/index.ts
@@ -1,24 +1,26 @@
-// var normalizedPath = require("path").join(__dirname);
+import * as path from 'path';
+import * as fs from 'fs';
+var normalizedPath = path.join(__dirname);
 
-// const data = {};
-// require("fs").readdirSync(normalizedPath).forEach(function (file) {
-//     if (file !== "index.ts") {
-//         Object.assign(data, require(`./${file}`));
-//     }
-// });
+const data = {};
+fs.readdirSync(normalizedPath).forEach(function (file) {
+    if (file !== "index.ts") {
+        Object.assign(data, require(`./${file}`));
+    }
+});
 
-// export default data
+export default data
 
-export * from './Character'
-export * from './Comic'
-export * from './Creator'
-export * from './DateDescriptor'
-export * from './DateTime'
-export * from './Event'
-export * from './MarvelNode'
-export * from './MarvelUrl'
-export * from './Query'
-export * from './Series'
-export * from './Story'
-export * from './Summary'
-export * from './TextObject'
+// export * from './Character'
+// export * from './Comic'
+// export * from './Creator'
+// export * from './DateDescriptor'
+// export * from './DateTime'
+// export * from './Event'
+// export * from './MarvelNode'
+// export * from './MarvelUrl'
+// export * from './Query'
+// export * from './Series'
+// export * from './Story'
+// export * from './Summary'
+// export * from './TextObject'
